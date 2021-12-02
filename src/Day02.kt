f9a1cd499b27c31e3884fbf14c5426d342523799
Miguel Panelo <miguelrpanelo@gmail.com>
2021-12-02 13:57:55 +0100

    Advent of code day 2

---
+++ src/Day02.kt
@@ -0,0 +1,55 @@
+enum class Direction {
+    FORWARD, DOWN, UP
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        var horizontal = 0
+        var depth = 0
+
+        input.forEach { movement ->
+            val split = movement.split(" ")
+            val direction = Direction.valueOf(split.first().uppercase())
+            val distance = split.last().toInt()
+
+            when (direction) {
+                Direction.FORWARD -> horizontal += distance
+                Direction.DOWN -> depth += distance
+                Direction.UP -> depth -= distance
+            }
+        }
+
+        return horizontal * depth
+    }
+
+    fun part2(input: List<String>): Int {
+        var aim = 0
+        var horizontal = 0
+        var depth = 0
+
+        input.forEach { movement ->
+            val split = movement.split(" ")
+            val direction = Direction.valueOf(split.first().uppercase())
+            val distance = split.last().toInt()
+
+            when (direction) {
+                Direction.FORWARD -> {
+                    horizontal += distance
+                    depth += (aim * distance)
+                }
+                Direction.DOWN -> {
+                    aim += distance
+                }
+                Direction.UP -> {
+                    aim -= distance
+                }
+            }
+        }
+
+        return horizontal * depth
+    }
+
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 1698735)
+    check(part2(testInput) == 1594785890)
+}
